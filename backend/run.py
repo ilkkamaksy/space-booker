25196cdb8a6987160a35d860742a9224d582e554
ilkkamaksy <ijmakine@gmail.com>
2021-01-29 16:07:57 +0200

    disable db tables auto gen

--- backend/run.py
+++ backend/run.py
@@ -21,9 +21,9 @@ app.config['SECRET_KEY'] = 'super-secret'
 
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity) 
 
