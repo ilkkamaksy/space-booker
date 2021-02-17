a63a65e93046b8969a0fa6586d7d66f666f5549e
ilkkamaksy <ijmakine@gmail.com>
2021-02-17 19:12:00 +0200

    catch all route

--- backend/run.py
+++ backend/run.py
@@ -40,9 +40,13 @@ api.add_resource(Booking, '/bookings', endpoint='booking_resource')
 api.add_resource(BookingSingular, '/bookings/<int:id>', endpoint='booking_singular_resource')
 api.add_resource(BookingList, '/bookings/account/<int:account_id>', endpoint='bookings_by_account')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def all_routes(path):
+    if not(path.startswith('api')):
+        return render_template('index.html')
+    else:
+        return redirect(url_for('404_error'))
 
 @app.route('/api/v1/me')
 @jwt_required()
